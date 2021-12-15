5caf8814514fea57331237a2aff7d5390c24427e
Marco Seghesso <mseghesso@gmail.com>
2021-12-16 05:49:23 +1300

    dinamically displaying list, refactored components

--- react-complete-course/src/components/Expenses/Expenses.js
+++ react-complete-course/src/components/Expenses/Expenses.js
@@ -1,33 +1,30 @@
-import React from 'react';
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-   return (
-      <Card className='expenses'>
-         <ExpenseItem
-         title={props.item[0].title}
-         amount={props.item[0].amount}
-         date={props.item[0].date}
-         ></ExpenseItem>
-         <ExpenseItem
-         title={props.item[1].title}
-         amount={props.item[1].amount}
-         date={props.item[1].date}
-         ></ExpenseItem>
-         <ExpenseItem
-         title={props.item[2].title}
-         amount={props.item[2].amount}
-         date={props.item[2].date}
-         ></ExpenseItem>
-         <ExpenseItem
-         title={props.item[3].title}
-         amount={props.item[3].amount}
-         date={props.item[3].date}
-         ></ExpenseItem>
+  const [filteredYear, setFilteredYear] = useState("2021");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.item.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses}/>
       </Card>
-   );
-}
+    </div>
+  );
+};
 
 export default Expenses;
